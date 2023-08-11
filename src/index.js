fb4d09563c80b47bd50d91681d80822f20a4712c
Igor Samgin <boolean.pg.me@gmail.com>
2023-08-11 14:20:20 +0200

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const abs = Math.abs(n);
+    const str = abs.toString();
+    let result = [];
+    for (let i = str.length - 1; i >= 0; i -= 1) {
+      result.push(str[i]);
+    }
+    return Number(result.join(''));
 }
